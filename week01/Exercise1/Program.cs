160689d48c2d01bab464fb4c7edde81f4958a775
miasoria <miasoriaa@gmail.com>
2025-07-19 10:15:35 -0300

    week01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        string name = Console.ReadLine();
+        
+        Console.Write("What is your second name?");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {surname}, {name} {surname}");
     }
 }
